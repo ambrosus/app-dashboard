49e47e1f9832489d8ad5790d43fcc93876f9abc2
gettingold@mail.ru <bit.size.dev@gmail.com>
2018-05-28 12:31:55 +0300

    Heroku node.js server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.set('port', PORT);
+
+const server = http.createServer(app);
+server.listen(PORT, () => { console.log('Server running.') });
