aeb0cccf0b1e01780e964a8de7a8148aaa64bdf5
Lazar Eric <lazareric.com@gmail.com>
2018-06-18 12:04:33 +0300

    QR added.

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from 'app/modules/dashboard/dashboard-routing.module';
 import { AssetComponent } from 'app/modules/dashboard/asset/asset.component';
-import {SharedModule} from "app/shared/shared.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import { SharedModule } from 'app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AssetsComponent } from 'app/modules/dashboard/assets/assets.component';
-import {AssetAddComponent} from "./asset-add/asset-add.component";
-import {RouterModule} from "@angular/router";
+import { AssetAddComponent } from './asset-add/asset-add.component';
+import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EventComponent } from './event/event.component';
 import { EventAddComponent } from './event-add/event-add.component';
-import {AssetsService} from 'app/services/assets.service';
-import {StorageService} from '../../services/storage.service';
+import { AssetsService } from 'app/services/assets.service';
+import { StorageService } from '../../services/storage.service';
+import { QRCodeModule } from 'angular2-qrcode';
 
 @NgModule({
   imports: [
@@ -20,7 +21,8 @@ import {StorageService} from '../../services/storage.service';
     DashboardRoutingModule,
     SharedModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    QRCodeModule
   ],
   declarations: [
     AssetComponent,
@@ -30,9 +32,6 @@ import {StorageService} from '../../services/storage.service';
     EventComponent,
     EventAddComponent
   ],
-  providers: [
-    AssetsService,
-    StorageService
-  ]
+  providers: [AssetsService, StorageService]
 })
-export class DashboardModule { }
+export class DashboardModule {}
