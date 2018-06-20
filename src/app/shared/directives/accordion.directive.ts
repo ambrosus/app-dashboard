6c52411380c2077174561337ebc39541267ad5b0
Lazar Eric <lazareric.com@gmail.com>
2018-06-20 14:26:16 +0300

    Accordions with arrows.

--- src/app/shared/directives/accordion.directive.ts
+++ src/app/shared/directives/accordion.directive.ts
@@ -8,11 +8,14 @@ export class AccordionDirective {
 
   @HostListener('click')
   onClick() {
+    const element = this.el.nativeElement;
     const nextSibling = this.el.nativeElement.nextElementSibling;
     if (!nextSibling.classList.contains('active')) {
       this.renderer.addClass(nextSibling, 'active');
+      this.renderer.addClass(element, 'active');
     } else {
       this.renderer.removeClass(nextSibling, 'active');
+      this.renderer.removeClass(element, 'active');
     }
   }
 }
