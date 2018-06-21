3e346c49cb01f1b662cb2c32b14ffec0cd87aed7
Lazar Eric <lazareric.com@gmail.com>
2018-06-21 16:58:00 +0300

    Help page done + sticky sidebar.

--- src/app/core/components/help/help.component.ts
+++ src/app/core/components/help/help.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-help',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
+  constructor(private el: ElementRef) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  scroll(element: String) {
+    const id = `#${element}`;
+    const el = this.el.nativeElement.querySelector(id);
+    el.scrollIntoView();
   }
-
 }
