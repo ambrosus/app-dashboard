5e0787067c48cd2ee02d2b78be3214e37869ee02
Meet Dave <meet.dave@outlook.com>
2018-07-06 14:23:42 +0530

    Password strength algo modified

--- src/app/services/password.service.ts
+++ src/app/services/password.service.ts
@@ -13,70 +13,34 @@ export class PasswordService {
   value: String;
   constructor() {}
 
-  strengthCalculator(input) {
-    this.value = input;
-    // Additions :-D
-    const noc = this.value.length; // Number of Characters
-    const nuc = this.value.replace(/[^A-Z]/g, '').length; // Uppercase Letters
-    const nlc = this.value.replace(/[^a-z]/g, '').length; // Lowercase Letters
-    const num = this.value.replace(/[^0-9]/g, '').length; // Numbers
-    let symr: number;
-    const sym = this.value.match(/[ !@#$£%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g); // Symbols
-    if (!sym) {
-      symr = 0;
-    } else {
-      symr = sym.length;
+  strengthCalculator(p: string) {
+    let _force = 0;
+    const _regex = /[$-/:-?{-~!"^_`\[\]]/g; // "
+    const _lowerLetters = /[a-z]+/.test(p);
+    const _upperLetters = /[A-Z]+/.test(p);
+    const _numbers = /[0-9]+/.test(p);
+    const _symbols = _regex.test(p);
+
+    const _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];
+
+    let _passedMatches = 0;
+    for (const _flag of _flags) {
+      _passedMatches += _flag === true ? 1 : 0;
     }
 
-    // Deductions :-(
-    let aucr: number; // Letters Only Resolver
-    const auc = this.value === this.value.toUpperCase();
-    if (auc === false) {
-      aucr = noc;
-    } else {
-      aucr = 0;
-    } // Letters Only
-    let anvr: number; // Number Only Resolver
-    const anv = +this.value;
-    if (anv !== NaN || anv !== 0) {
-      anvr = noc;
-    } else {
-      anvr = 0;
-    } // Numbers Only
-    let cons: number; // Repeat Characters Resolver
-    if (this.value.match(/(.)\1\1/)) {
-      cons = noc * noc;
-    } else {
-      cons = 0;
-    } // Repeat Characters
-    // The MF math
-    const additions = ((noc * 4) + ((noc - nuc) * 2) + ((nlc - nuc) * 2) + (num * 4) + ((symr) * 6));
-    const deductions = ((aucr) + (anvr) + cons);
-    const total = additions - deductions;
-    if (sym == null) {
-      this.symbool['isit'] = false;
-    } else {
-      this.symbool['isit'] = true;
-    }
-    if (nuc === 0) {
-      this.nucbool['isit'] = false;
-    } else {
-      this.nucbool['isit'] = true;
-    }
-    if (num === 0) {
-      this.numbool['isit'] = false;
-    } else {
-      this.numbool['isit'] = true;
-    }
-    if (total < 101) {
-      if (total < 0) {
-        return this.width = 1;
-      } else {
-        return this.width = total;
-      }
-    } else {
-      return this.width = 100;
-    }
+    _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
+    _force += _passedMatches * 10;
+
+    // penality (short password)
+    _force = (p.length <= 6) ? Math.min(_force, 10) : _force;
+
+    // penality (poor variety of characters)
+    _force = (_passedMatches === 1) ? Math.min(_force, 25) : _force;
+    _force = (_passedMatches === 2) ? Math.min(_force, 60) : _force;
+    _force = (_passedMatches === 3) ? Math.min(_force, 100) : _force;
+
+    console.log(_force);
+    return _force;
   }
 
 }
