6035bd859c5fbda00f464e1bc354a01602afab57
Lazar Eric <lazareric.com@gmail.com>
2018-07-19 12:36:50 +0300

    Moving switching to header, in progress.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -12,11 +12,34 @@ export class HeaderComponent implements OnInit {
   isLoggedin = false;
   greeting = 'Hi, welcome!';
   overlay = false;
+  accounts;
+  currentAccount;
+  addAccount;
 
-  constructor(private auth: AuthService, private storage: StorageService) {}
+  constructor(private auth: AuthService,
+              private storage: StorageService) {}
 
   ngOnInit() {
+    this.headerInit();
+  }
+
+  headerInit() {
     this.greeting = this.storage.get('full_name') || 'Hi, welcome!';
     this.isLoggedin = JSON.parse(this.storage.get('isLoggedin'));
+    const accounts = this.storage.get('accounts');
+    this.accounts = accounts ? JSON.parse(accounts) : [];
+    this.currentAccount = this.accounts[0];
+  }
+
+  switchAccount(address) {
+    this.auth.switchAccount(address);
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+  logoutAll() {
+    this.auth.logoutAll();
   }
 }
