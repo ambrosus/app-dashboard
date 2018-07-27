d21e8032540042be2b19f39ba53f38c67faebaf0
Lazar Eric <lazareric.com@gmail.com>
2018-07-27 13:50:04 +0300

    QR code has logo in the middle.

--- src/app/shared/components/qr-code/qr-code.component.ts
+++ src/app/shared/components/qr-code/qr-code.component.ts
@@ -8,7 +8,6 @@ declare let QRCode: any;
   styleUrls: ['./qr-code.component.scss']
 })
 export class QrCodeComponent implements OnInit {
-
   @Input() value;
   @Input() size;
   @Input() logo;
@@ -20,14 +19,44 @@ export class QrCodeComponent implements OnInit {
   }
 
   generateQR() {
-    QRCode.toDataURL(this.value, { errorCorrectionLevel: 'L' }).then(url => {
+    const canvas = document.createElement('canvas');
+
+    QRCode.toCanvas(canvas, this.value, { errorCorrectionLevel: 'M' })
+      .then(c => {
+        this.drawLogo(c).then((_c: any) => {
+          const img = document.createElement('img');
+          img.src = _c.toDataURL();
+          img.width = this.size ? this.size : 300;
+          img.height = this.size ? this.size : 300;
+
+          this.renderer.appendChild(this.el.nativeElement, img);
+        });
+      })
+      .catch(err => {
+        console.log('QRCode generate error: ', err);
+      });
+  }
+
+  drawLogo(canvas) {
+    return new Promise((resolve, reject) => {
+      if (this.logo) {
         const img = document.createElement('img');
-        img.src = url;
-        img.width = this.size ? this.size : 300;
-        img.height = this.size ? this.size : 300;
-        this.renderer.appendChild(this.el.nativeElement, img);
-    }).catch(err => {
-      console.log('QRCode generate error: ', err);
+        img.src = this.logo;
+        img.onload = () => {
+          const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
+          const width = 40;
+          const height = 40;
+
+          const x = (canvas.width - width) / 2;
+          const y = (canvas.height - height) / 2;
+
+          ctx.drawImage(img, x, y, width, height);
+          resolve(canvas);
+        };
+      } else {
+        resolve(canvas);
+      }
     });
   }
 }
