67d386ebe500b204e65c2c0022e23c7533fd2065
Meet Dave <meet.dave@outlook.com>
2018-07-30 22:24:51 +0530

    Name change to incorporate 4 charts

--- src/app/modules/dashboard/chart/chart.component.ts
+++ src/app/modules/dashboard/chart/chart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
 import { ChartEvent, ChartType } from './chartist/chartist.component';
-const data: any = require('./lineData.json');
+const data: any = require('./chartData.json');
 declare var require: any;
 
 export interface Chart {
@@ -19,13 +19,26 @@ export interface Chart {
 })
 export class ChartComponent implements OnInit {
 
-  chart: any = {};
+  chart1: any = {};
+  chart2: any = {};
+  chart3: any = {};
+  chart4: any = {};
 
   constructor() { }
 
   ngOnInit() {
-    this.chart['data'] = data.Line;
-    this.chart['type'] = 'Line';
+    this.chart1['data'] = data.Line;
+    this.chart1['type'] = 'Line';
+
+    this.chart2['data'] = data.LineWithArea;
+    this.chart2['type'] = 'Line';
+
+    this.chart3['data'] = data.BiPolarBar;
+    this.chart3['type'] = 'Bar';
+
+    this.chart4['data'] = data.Pie;
+    this.chart4['type'] = 'Pie';
+
   }
 
 }
