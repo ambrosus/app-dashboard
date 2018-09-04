d8d5609b85b6dd3911319b8ff5e9f8d881e9e0d9
Meet Dave <meet.dave@outlook.com>
2018-09-04 10:16:49 +0530

    Security settings page

---
+++ src/app/modules/settings/components/pages/security-settings/security-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-security-settings',
+  templateUrl: './security-settings.component.html',
+  styleUrls: ['./security-settings.component.scss']
+})
+export class SecuritySettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
