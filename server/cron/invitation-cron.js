e960b95de7536eb342b183aefff7fdcfb83010e9
lazar <lazareric.com@gmail.com>
2018-09-04 12:01:04 +0300

    CRON refactor with moment.js

--- server/cron/invitation-cron.js
+++ server/cron/invitation-cron.js
@@ -1,20 +1,21 @@
 const CronJob = require('cron').CronJob;
+const moment = require('moment');
+
 const Invites = require('./../models/invites');
 
 const job = new CronJob('0 */480 * * * *', () => {
-    // Cron job runs every 480 minutes = Every 8 hours 
-    // 3 times a day 
-	const now = Math.floor(Date.now());
-    Invites.find()
-    .then(results => {
-        results.forEach(result => {
-            if (result.validUntil <= now) {
-                Invites.deleteOne({_id: result._id})
-                    .then(result => console.log(result))
-                    .catch(error => console.log(error));
-            }
-        });
-    }).catch(error => (reject(error)))
+  // Cron job runs every 480 minutes = Every 8 hours
+  // 3 times a day
+  Invites.find()
+    .then(invites => {
+      invites.forEach(invite => {
+        if (moment().diff(invite.validUntil) < 0) {
+          Invites.findByIdAndRemove(invite._id)
+            .then(deleted => console.log('Invite expired: ', deleted))
+            .catch(error => console.log('Invite delete error: ', error));
+        }
+      });
+    }).catch(error => console.log('CRON Invites find error: ', error));
 });
 
 job.start();
