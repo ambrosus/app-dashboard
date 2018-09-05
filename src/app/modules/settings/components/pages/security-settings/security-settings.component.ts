e662fdb55f489a1f3a7135bc6c6765a53729ba53
lazar <lazareric.com@gmail.com>
2018-09-05 11:53:42 +0300

    Securit tab fix

--- src/app/modules/settings/components/pages/security-settings/security-settings.component.ts
+++ src/app/modules/settings/components/pages/security-settings/security-settings.component.ts
@@ -1,28 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UsersService } from 'app/services/users.service';
 import { StorageService } from 'app/services/storage.service';
 import { HttpClient } from '@angular/common/http';
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-security-settings',
   templateUrl: './security-settings.component.html',
   styleUrls: ['./security-settings.component.scss']
 })
-export class SecuritySettingsComponent implements OnInit {
-
+export class SecuritySettingsComponent implements OnInit, OnDestroy {
   sessions;
-  spinner: Boolean = true;
+  spinner: Boolean = false;
   resetForm: FormGroup;
   resetSuccess: Boolean = false;
   error;
+  getSessionsSub: Subscription;
+  logoutSessionSub: Subscription;
 
-  constructor(
-    private http: HttpClient,
-    private userService: UsersService,
-    private storage: StorageService
-  ) {
+  constructor(private http: HttpClient, private storage: StorageService) {
     this.resetForm = new FormGroup({
       oldPassword: new FormControl(null, [Validators.required]),
       password: new FormControl(null, [Validators.required]),
@@ -30,36 +26,40 @@ export class SecuritySettingsComponent implements OnInit {
     });
   }
 
-
   ngOnInit() {
+    this.getSessions();
+  }
+
+  getSessions() {
     const email = this.storage.get('user')['email'];
-    this.http.get(`/api/auth/sessions/${email}`).subscribe(
+    const url = `/api/auth/sessions/${email}`;
+
+    this.getSessionsSub = this.http.get(url).subscribe(
       resp => {
-        console.log(resp);
+        console.log('GET sessions: ', resp);
         this.sessions = resp;
-        this.spinner = false;
       },
-      err => {
-        console.log(err);
-      }
+      err => console.log('GET sessions error: ', err)
     );
   }
 
   logoutSession(sessionId) {
-    this.http.delete(`/api/auth/session/${sessionId}`).subscribe(
-      resp => {
-        this.ngOnInit();
-      },
-      err => {
-        console.log(err);
-      }
+    const url = `/api/auth/session/${sessionId}`;
+
+    this.logoutSessionSub = this.http.delete(url).subscribe(
+      resp => this.getSessions(),
+      err => console.log('DELETE session error: ', err)
     );
+  }
+
+  ngOnDestroy() {
+    if (this.getSessionsSub) { this.getSessionsSub.unsubscribe(); }
+    if (this.logoutSessionSub) { this.logoutSessionSub.unsubscribe(); }
   }
 
   changePassword() {
     this.resetErrors();
     const email = this.storage.get('user')['email'];
-    console.log(email);
     const newPassword = this.resetForm.get('password').value;
     const oldPassword = this.resetForm.get('oldPassword').value;
     const passwordConfirm = this.resetForm.get('passwordConfirm').value;
@@ -71,13 +71,14 @@ export class SecuritySettingsComponent implements OnInit {
 
     this.spinner = true;
 
+    const url = `/api/users/password`;
     const body = {
       email,
       oldPassword,
       newPassword
     };
 
-    this.http.put('/api/users/password', body).subscribe(
+    this.http.put(url, body).subscribe(
       resp => {
         this.spinner = false;
         this.resetSuccess = true;
@@ -94,5 +95,4 @@ export class SecuritySettingsComponent implements OnInit {
     this.error = false;
     this.resetSuccess = false;
   }
-
 }
