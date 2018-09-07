d376742eb47de48c51b9e52b5b02147e24d4756b
Meet Dave <meet.dave@outlook.com>
2018-09-07 10:50:53 +0530

    Add chai-http to make http requests to routes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,37 @@
 const chai = require('chai');
-const AuthController = require('../server/controllers/auth');
+const chaiHttp = require('chai-http');
+const server = 'http://localhost:5000';
 
+chai.use(chaiHttp);
 const expect = chai.expect;
+const should = chai.should()
 
-describe('Given an instance of AuthController', () => {
-    describe('see if controller is initialised', () => {
-      it('console.log the controller', () => {
-        console.log(AuthController.getActiveSessions);
+describe('Given an instance of UserController', () => {
+
+    describe('GET /api/user/settings/:email', () => {
+      it('Should get the user settings ', (done) => {
+        chai.request(server)
+          .get('/api/users/settings/meet.dave@outlook.com')
+          .end((err, response) => {
+            if (!err) {
+              response.body.should.be.a('object');
+              done();
+            }
+          })
       });
     });
+
+    describe('GET /api/users/settings/:email', () => {
+      it('Should get the user settings ', (done) => {
+        chai.request(server)
+          .get('/api/users/meet.dave@outlook.com')
+          .end((err, response) => {
+            if (!err) {
+              response.body.should.be.a('object');
+              done();
+            }
+          })
+      });
+    });
+
 });
