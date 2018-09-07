1d800bcc32d612da258e0643fa4e82d20aabb877
lazar <lazareric.com@gmail.com>
2018-09-07 12:30:04 +0300

    Footer public and help sidebar

---
+++ src/app/shared/components/footer-public/footer-public.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-public',
+  templateUrl: './footer-public.component.html',
+  styleUrls: ['./footer-public.component.scss']
+})
+export class FooterPublicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
