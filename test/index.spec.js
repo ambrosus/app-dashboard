2d7790a5ea94aa9d9c5e5aee0ced26f946ede8ad
Meet Dave <meet.dave@outlook.com>
2018-09-10 14:28:30 +0530

    Split tests into multiple files

--- test/index.spec.js
+++ test/index.spec.js
@@ -35,20 +35,3 @@ describe('Given an instance of UserController', () => {
   });
 
 });
-
-describe('Given an instance of HermesesController', () => {
-
-  describe('GET /api/hermeses', () => {
-    it('Should get the hermeses array', (done) => {
-      chai.request(server)
-        .get('/api/hermeses')
-        .end((err, response) => {
-          response.body.should.have.property('resultCount');
-          response.should.have.status(200);
-          response.body.should.be.a('object');
-          done();
-        })
-    });
-  });
-
-});
