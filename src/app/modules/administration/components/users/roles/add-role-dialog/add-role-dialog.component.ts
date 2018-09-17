d05a5621f7a50a14ba3fd77345f76f26ee9d1d4a
Meet Dave <meet.dave@outlook.com>
2018-09-17 18:01:33 +0530

    Save new role with permissions

--- src/app/modules/administration/components/users/roles/add-role-dialog/add-role-dialog.component.ts
+++ src/app/modules/administration/components/users/roles/add-role-dialog/add-role-dialog.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-role-dialog',
   templateUrl: './add-role-dialog.component.html',
   styleUrls: ['./add-role-dialog.component.scss']
 })
-export class AddRoleDialogComponent implements OnInit {
+export class AddRoleDialogComponent {
 
   spinner: Boolean = false;
   roleForm: FormGroup;
   createPromise;
   title: string;
   permissions: string;
+  selectedPermissions: string[] = [];
+  error: string;
+  successMessage: string;
 
   permissionsArray = [
     { id: '1', name: 'Invites', value: 'invites' },
     { id: '2', name: 'Users', value: 'users' }
   ]
 
-  constructor(private dialogRef: MatDialogRef<AddRoleDialogComponent>) {
+  constructor(private dialogRef: MatDialogRef<AddRoleDialogComponent>, private http: HttpClient) {
     this.roleForm = new FormGroup({
-      title: new FormControl(null, [Validators.required]),
-      permissions: new FormControl(null, [Validators.required])
+      title: new FormControl(null, [Validators.required])
     });
-  }
-
-  ngOnInit() {
   }
 
   closeDialog() {
@@ -35,17 +35,32 @@ export class AddRoleDialogComponent implements OnInit {
   }
 
   create(){
+    this.error = null;
+    this.successMessage = null;
     this.createPromise = new Promise((resolve, reject) => {
-      setTimeout(reject, 2000);
+      if (this.selectedPermissions.length === 0) { this.error = 'Please select at least one permission'; reject(); }
+      else if (!this.roleForm.valid) { this.error = 'Please fill the role title'; reject(); }
+      else {
+        const body = { 'title': this.roleForm.get('title').value, 'permissions': this.selectedPermissions };
+        const url = `/api/roles`;
+    
+        this.http.post(url, body).subscribe(
+          (resp: any) => {
+            this.successMessage = 'New role created successfully!';
+            resolve();
+        },
+        err => {
+          reject();
+          this.error = err.error ? err.error.message : JSON.stringify(err);
+          console.log('Role save failed: ', err);
+        });
+      }
     });
   }
 
-  addRole() {
-    console.log('Add Role');
-  }
-
-  selectPermission(id) {
-    console.log(id);
+  selectPermission(value) {
+    if (this.selectedPermissions.indexOf(value) > -1) { this.selectedPermissions = this.selectedPermissions.filter(a => a !== value) } 
+    else { this.selectedPermissions.push(value); }
   }
 
 }
