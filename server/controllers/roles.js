d2beb8d51f466f60e26632a32193a1a1d847b8d1
Meet Dave <meet.dave@outlook.com>
2018-09-18 15:44:05 +0530

    Roles API Changes
    Removed editPermission API Endpoint
    Changed create POST method to create PUT
    roles/api {PUT} endpoint now supports editing a Role if the _id is passed

--- server/controllers/roles.js
+++ server/controllers/roles.js
@@ -11,26 +11,37 @@ const Role = require('../models/roles');
 /**
  * This route allows you to create a new Role and add a set of permissions to them
  *
- * @name createRole
- * @route {POST} api/roles
+ * @name createOrEditRole
+ * @route {PUT} api/roles
  * @bodyparam 
  *      - title (e.g: { title: 'owner' }), 
  *      - permissions (e.g: { permissions: "string1, string2" }),
- *      - id: Number
  * @returns Status code 400 on failure
  * @returns New role saved successfully message on success with status code 200
  */
 exports.create = (req, res, next) => {
-  const role = new Role();
-
+  const _id = req.body._id;
   const title = req.body.title;
   const permissions = req.body.permissions;
 
-  if (title && permissions) {
-    role._id = new mongoose.Types.ObjectId(),
-    role.title = req.body.title;
-    role.permissions = req.body.permissions;
-    role.createdBy = req.session.user._id;
+  if (_id && title && permissions) {
+
+    Role.findOneAndUpdate({ _id }, {title: title, permissions: permissions})
+      .then(updateResponse => {
+        if (updateResponse) {
+          req.status = 200;
+          req.json = { message: 'Permissions updated successfully' }
+          return next();
+        }
+      }).catch(error => (console.log(error), res.status(400).json({ message: error })));
+
+  } else if (!_id && title && permissions) {
+    const role = new Role({
+      _id: new mongoose.Types.ObjectId(),
+      title,
+      permissions,
+      createdBy: req.session.user._id
+    });
     role.save()
       .then(saveResponse => {
         req.status = 200;
@@ -40,40 +51,7 @@ exports.create = (req, res, next) => {
       .catch(error => (console.log(error), res.status(400).json({ message: error })));
   } else if (!title) {
     return res.status(400).json({ message: 'Role "title" is required' });
-  } else if (!id) {
-    return res.status(400).json({ message: 'Role "id" is required' });
   } else if (!permissions) {
-    return res.status(400).json({ message: 'Role "permissions" is required' });
-  }
-
-};
-
-/**
- * User can edit the permissions for a particular role ID 
- *
- * @name editPermissions
- * @route {PUT} api/roles/:id
- * @bodyparam roleID, permissions (e.g: { "permissions": ["invites", "roles"] })
- * @returns Status code 400 on failure
- * @returns Permissions updated message on success with status code 200
- */
-exports.editPermissions = (req, res, next) => {
-  const update = {};
-  const _id = req.params.id;
-  update.permissions = req.body.permissions;
-
-  if (_id && update.permissions) {
-    Role.findOneAndUpdate({ _id }, update)
-      .then(updateResponse => {
-        if (updateResponse) {
-          req.status = 200;
-          req.json = { message: 'Permissions updated successfully' }
-          return next();
-        }
-      })
-  } else if (!_id) {
-    return res.status(400).json({ message: 'Role "_id" is required' });
-  } else if (!update.permissions) {
     return res.status(400).json({ message: 'Role "permissions" is required' });
   }
 
