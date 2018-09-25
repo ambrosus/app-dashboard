bdb049396a955fd075b158c5b17173e7a06fc4df
Meet Dave <meet.dave@outlook.com>
2018-09-25 21:28:02 +0530

    Generate user sidebar UI with respect to roles assigned WIP

--- src/app/modules/administration/components/users/users/users.component.ts
+++ src/app/modules/administration/components/users/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'app/services/users.service';
 
 @Component({
   selector: 'app-users',
@@ -9,28 +10,49 @@ export class UsersComponent implements OnInit {
   sidebar = [
     {
       title: 'All users',
-      link: 'all',
-      icon: 'users'
+      link: 'users',
+      icon: 'users',
+      role: 'users'
     },
     {
       title: 'Invite members',
       link: 'invite',
-      icon: 'user-plus'
+      icon: 'user-plus',
+      role: 'invites'
     },
     {
       title: 'Invites',
       link: 'invites',
-      icon: 'user-check'
+      icon: 'user-check',
+      role: 'invites'
     },
     {
       title: 'Roles',
       link: 'roles',
-      icon: 'user-check'
+      icon: 'user-check',
+      role: 'roles'
     }
   ];
 
-  constructor() { }
+  userSidebar = [];
+  permissions: string[];
+
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.permissions = ['invites', 'users', 'roles'];
+    if (this.permissions.length) {
+      this.generateSidebar();
+    } else {
+      this.userSidebar = this.sidebar;
+    }
   }
+
+  generateSidebar() {
+    this.userSidebar = this.sidebar.filter(module => {
+      const validPermission = this.permissions.filter(p => p === module['role']);
+      if (validPermission.length !== 0 ) { return module; }
+    });
+  }
+
 }
