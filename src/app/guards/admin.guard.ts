bbb7c74c7ff9fa77037cba26394a86e5b3541dff
Meet Dave <meet.dave@outlook.com>
2018-09-26 15:29:12 +0530

    Admin guard added

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -2,22 +2,26 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from 'app/services/auth.service';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpResponse } from './response.interface';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private router: Router, private auth: AuthService) { }
+  constructor(private router: Router, private auth: AuthService, private http: HttpClient) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
 
     console.log('canActivate@AdminGuard');
 
     return new Promise(resolve => {
-      if (this.auth.isLoggedIn()) {
-        resolve(true);
-      } else {
-        this.router.navigate(['/assets']);
-        resolve(false);
-      }
+      this.http.get('/api/users').subscribe((res: HttpResponse) => {
+        if (res.data[0].role && res.data[0].role.permissions) {
+          resolve(true);
+        } else {
+          this.router.navigate(['/assets']);
+          resolve(false);
+        }
+      });
     });
   }
 }
