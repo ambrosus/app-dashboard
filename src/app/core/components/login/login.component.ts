bbf49d7e7aa947139acc413e10ff2099183f3686
Lazar Eric <lazareric@Macs-MBP.lan>
2018-11-03 12:35:19 +0200

    Added valid sings on inputs

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { ViewEncapsulation } from '@angular/compiler/src/core';
 import { MatDialog } from '@angular/material';
 import { AuthService } from 'app/services/auth.service';
+
+declare let Web3: any;
 
 @Component({
   selector: 'app-login',
@@ -16,7 +18,6 @@ export class LoginComponent implements OnInit {
     loginForm?: FormGroup,
     privateKeyForm?: FormGroup
   } = {};
-
   error;
   deviceInfo;
   promiseAction;
@@ -32,18 +33,26 @@ export class LoginComponent implements OnInit {
       password: new FormControl(null, [Validators.required]),
     });
     this.forms.privateKeyForm = new FormGroup({
-      privateKey: new FormControl(null, [Validators.required]),
+      privateKey: new FormControl(null, [Validators.required, this.validatePrivateKey]),
     });
   }
 
   ngOnInit() { }
+
+  validatePrivateKey(control: AbstractControl) {
+    try {
+      const web3 = new Web3();
+      console.log(web3.eth.accounts.privateKeyToAccount(control.value).address);
+      return null;
+    } catch (e) { return { 'Invalid private key': control.value }; }
+  }
 
   verifyAccount() {
     this.error = false;
     this.forgotPassword = false;
     const data = this.forms.privateKeyForm.value;
 
-    if (this.forms.privateKeyForm.invalid) { return this.error = { privateKey: 'Private key is required' }; }
+    if (this.forms.privateKeyForm.invalid) { return this.error = 'Fill all required fileds'; }
 
     this.promiseAction = new Promise((resolve, reject) => {
       this.authService.verifyAccount(data.privateKey).subscribe((resp: any) => {
--- src/app/shared/directives/error.directive.ts
+++ src/app/shared/directives/error.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, Input, ElementRef, OnChanges, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, Input, ElementRef, OnChanges, HostListener, OnInit, Renderer2, OnDestroy } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appError]',
 })
-export class ErrorDirective implements OnChanges, OnInit {
+export class ErrorDirective implements OnChanges, OnInit, OnDestroy {
+  statusSub: Subscription;
   error;
   parent;
   controlName;
+  errors;
+  form;
+  hideValidSign;
 
-  @Input() appError: any;
+  @Input() appError: [string, FormGroup, boolean?];
 
   constructor(
     private el: ElementRef,
@@ -16,14 +22,31 @@ export class ErrorDirective implements OnChanges, OnInit {
   ) { }
 
   ngOnInit() {
+    [this.errors, this.form, this.hideValidSign] = this.appError;
     this.parent = this.el.nativeElement.parentNode;
     this.controlName = this.el.nativeElement.attributes['formcontrolname'].nodeValue;
+    // Valid sign and class
+    this.statusSub = this.form.get(this.controlName).statusChanges.subscribe(
+      status => {
+        if (status === 'VALID') {
+          if (this.hideValidSign) { this.renderer.addClass(this.parent, 'no-sign'); }
+          this.renderer.addClass(this.parent, 'valid');
+        } else {
+          this.renderer.removeClass(this.parent, 'valid');
+          this.renderer.removeClass(this.parent, 'no-sign');
+        }
+      },
+    );
   }
 
   ngOnChanges() {
-    if (this.appError && this.appError[this.controlName] && this.el.nativeElement.classList.contains('ng-invalid')) {
+    if (this.errors && this.el.nativeElement.classList.contains('ng-invalid')) {
       this.createError();
     } else { this.removeError(); }
+  }
+
+  ngOnDestroy() {
+    if (this.statusSub) { this.statusSub.unsubscribe(); }
   }
 
   @HostListener('keyup', ['$event'])
@@ -34,15 +57,22 @@ export class ErrorDirective implements OnChanges, OnInit {
     } else { this.removeError(); }
   }
 
+  getErrorMessage() {
+    const controlErrors = this.form.get(this.controlName).errors;
+    return Object.keys(controlErrors).reduce((message, key, index, array) => {
+      if (controlErrors[key]) { message += message ? `, ${key}` : key; }
+      return message;
+    }, '');
+  }
+
   createError() {
-    if (!this.error) {
-      this.renderer.addClass(this.parent, 'error');
-      const message = document.createElement('p');
-      message.textContent = this.appError[this.controlName] || this.appError || '';
-      message.className = 'message';
-      this.renderer.appendChild(this.parent, message);
-      this.error = true;
-    }
+    if (this.error) { this.removeError(); }
+    this.renderer.addClass(this.parent, 'error');
+    const message = document.createElement('p');
+    message.textContent = this.getErrorMessage();
+    message.className = 'message';
+    this.renderer.appendChild(this.parent, message);
+    this.error = true;
   }
 
   removeError() {
