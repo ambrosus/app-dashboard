25cd92164b410e2a641a20467e3f887b9ebff956
Sergiy Chayka <schayka@gmail.com>
2019-09-17 05:48:35 -0400

    api url fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,11 @@ let ambrosus = false;
 
 try {
   core = location.hostname.split('.');
-  core.shift();
+  if (core.length === 3) {
+    core = [core[0].split('-')[1], ...core.slice(1)];
+  } else {
+    core.shift();
+  }
 
   if (['ambrosus', 'ambrosus-dev', 'ambrosus-test'].indexOf(core[core.length - 2]) > -1) {
     ambrosus = true;
