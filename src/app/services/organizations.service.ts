668374ce510bad38578974df7f67e4869bf3da41
Sergiy Chayka <schayka@gmail.com>
2019-09-17 07:05:43 -0400

    syntax fix

--- src/app/services/organizations.service.ts
+++ src/app/services/organizations.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
 
 interface Organization {
   _id?: string;
